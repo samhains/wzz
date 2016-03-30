f28662b20251094e588760ba8440a70f02f9f3f9
sdlovecraft <sdh@eml.cc>
2016-03-31 10:29:34 +1100

    remove node server

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    app = express();
-app.use(express.static(__dirname+'/public'));
-
-app.get('/', function(req, res){
-    res.sendFile('/index.html');
-});
-
-app.listen(4000);
